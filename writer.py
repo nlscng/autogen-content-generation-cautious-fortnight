019a42911a0dcd38709e9a246c62738d4f2326a8
ncheng <nlscng.g@gmail.com>
2025-10-31 21:41:58 -0500

    Start writer.py, where agents and configs for agents reside.

---
+++ writer.py
@@ -0,0 +1,21 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import asyncio
+import os
+
+async def main():
+    model = OpenAIChatCompletionClient(
+        model="gpt-4.1",
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+    writer_agent = AssistantAgent(
+        name="WriterAgent",
+        system_message=
+        """You are a writer agent. You will be given a topic, and you need to write
+        some content based on the topic. You will be collaborating with a content
+        critic agent, and an SEO critic agent. These agents will provide feedback
+        and score your content. You should address their feedback and improve your 
+        content based on their suggestions. Your goal is to produce high-quality
+        content that meets the criteria set by the critic agent and SEO critic agent.""",
+        model_client = model
+    )
