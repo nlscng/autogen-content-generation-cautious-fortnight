265127af5f62a1ba08445fc993bb3a3d6fedb30f
ncheng <nlscng.g@gmail.com>
2025-12-09 21:08:24 -0600

    Small update to UI, now we have a page.

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import asyncio
 from writer import teamConfig, orchetrate
+
+st.title("Agent Content Generation!!!")
+
+chat = st.container()
+
+prompt = st.chat_input("Enter a description of the content you want the team of agents to create")
+
+if prompt:
+    with chat:
+        st.write("Hey! Generating content...")
+        
